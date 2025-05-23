6989858a2915d14b834de295fc73610ca4f554b2
Ain-Jeong <ain.jeong@stu.ocsb.ca>
2025-05-23 13:56:03 +0000

    INIT

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Ain Jeong All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Ain Jeong
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+'use-strict'
+
+/**
+ * This function calculates exponents
+ */
+// eslint-disable-next-line no-unused-vars
+function calculateExponent () {
+  let counter = 0
+  let answer = 1
+
+  // input
+  const baseNumber = parseFloat(document.getElementById('base-number').value)
+  const exponentNumber = parseFloat(
+    document.getElementById('exponent-number').value
+  )
+
+  // process
+  while (counter < exponentNumber) {
+    counter = counter + 1
+    answer = answer * baseNumber
+  }
+  // output
+  document.getElementById('answer').innerHTML =
+    baseNumber + ' to the power of ' + exponentNumber + ' is: ' + answer + '.'
+}
